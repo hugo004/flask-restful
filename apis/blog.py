ac49bc77c96ab2c49d5b3ca4526a361aec940819
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2021-02-13 08:12:18 +0000

    feat: blog CRUD api

--- apis/blog.py
+++ apis/blog.py
@@ -1,8 +1,11 @@
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource, fields, reqparse
 from database.blog import table as BLOGS
+from bson.objectid import ObjectId
+from flask import request
+from datetime import datetime
 
 api = Namespace('blogs', description='Blogs related operations')
-blog = api.model('Blog', {
+blog = api.model('BlogList', {
   '_id': fields.String(required=True, description='The blog identifier'),
   'title': fields.String(required=True, description='Blog title'),
   'author': fields.String(required=True, description='The blog author'),
@@ -15,6 +18,65 @@ blog = api.model('Blog', {
   'date': fields.DateTime(description='The blog creation date')
 })
 
+blog_dto = api.model('BlogDetail', {
+  'title': fields.String(required=True, description='Blog title'),
+  'author': fields.String(required=True, description='The blog author'),
+  'content': fields.String(required=True, description='The blog\'s content'),
+  'likes': fields.Integer(description='Number of like'),
+  'shares': fields.Integer(description='Number of share'),
+  'public': fields.Boolean(description='Is the blog pulish'),
+  'tags': fields.List(fields.String, description="The blog tags"),
+  'category': fields.Integer(description='The blog category'),
+  'date': fields.DateTime(description='The blog creation date')
+})
+
+
+class BlogDTO(object):
+  def __init__(self):
+    self.title = None
+    self.author = None
+    self.content = None
+    self.likes = 0
+    self.shares = 0
+    self.public = False
+    self.tags= []
+    self.category = 0
+    self.date = None
+
+  def get(self, id):
+    blog = BLOGS.find_one({'_id': ObjectId(id)})
+    if blog:
+      return blog
+    api.abort(404, 'Blog {} doesn\'t exit'.format(id))
+  
+  def create(self, payload):
+    payload.update({
+      'date': datetime.utcnow().isoformat(),
+      'likes': 0,
+      'shares': 0
+    })
+    blog_id = BLOGS.insert_one(payload).inserted_id
+    
+    return BLOGS.find_one({'_id': blog_id})
+
+  def update(self, id, data):
+    if BLOGS.find_one({'_id': ObjectId(id)}):
+      BLOGS.update_one({
+        '_id': ObjectId(id)
+      }, {
+        '$set': data
+      })
+      return { 'success': True }
+      
+    api.abort(404, 'Blog {} doesn\'t exit'.format(id))
+
+  def delete(self,id):
+    if BLOGS.find_one({'_id': ObjectId(id)}):
+       BLOGS.delete_one({'_id': ObjectId(id)})
+       return { 'success': True }
+    api.abort(404, 'Blog {} doesn\'t exit'.format(id))
+
+DTO = BlogDTO()
 
 
 @api.route('/')
@@ -29,9 +91,12 @@ class BlogList(Resource):
       records.append(b)
     return records
 
+  @api.doc('create_blog')
+  @api.expect(blog_dto)
+  @api.marshal_with(blog, code=201)
   def post(self):
     '''Create a blog'''
-    pass
+    return DTO.create(api.payload), 201
   
 
 
@@ -40,19 +105,17 @@ class BlogList(Resource):
 @api.response(404, 'Blog not found')
 class Blog(Resource):
   @api.doc('get_blog')
-  @api.marshal_list_with(blog)
+  @api.marshal_with(blog)
   def get(self, id):
     '''Fetch a blog given its identifier'''
-    blog = BLOGS.find_one({'_id': id})
-    print(blog)
-    if blog:
-      return blog
-    api.abort(404)
+    return DTO.get(id)
 
+  @api.doc('update_blog')
+  @api.expect(blog_dto)
   def put(self, id):
     '''Update a blog with id'''
-    pass
+    return DTO.update(id, api.payload)
 
   def delete(self, id):
     '''Delete a blog with id'''
-    pass
+    return DTO.delete(id)
