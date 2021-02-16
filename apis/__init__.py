bbe5a102ae2a3decdcd226fb09de85a7177ae909
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2021-02-16 10:26:48 +0000

    feat: remove no use code

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,19 +1,5 @@
-# from flask_restx import Api
-# from .blog import api as blog_ns
-
-# api = Api(
-#   title='Hugo Swagger',
-#   version='1.0',
-#   description='A simple api'
-# )
-
-# api.add_namespace(blog_ns, path='/api/v1/blogs')
-
-
-
 from flask_restx import Api
 from flask import Blueprint
-from .foo import ns as foo_ns
 from .blog import api as blog_ns
 
 api_bp = Blueprint('api', __name__)
@@ -25,4 +11,3 @@ api = Api(
 )
 
 api.add_namespace(blog_ns)
-api.add_namespace(foo_ns)
